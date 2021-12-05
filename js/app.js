57af3e0bfd279913166424825a39095794751423
Alejandro <felipe_60@live.com.mx>
2021-12-04 23:31:56 -0600

    Change rouyte

--- js/app.js
+++ js/app.js
@@ -3,6 +3,6 @@ const BASE_URL = window.location.href
 
 if (navigator.serviceWorker) {
     console.log("Activado");
-    BASE_URL.startsWith('https:') ? url = '/20213-PWA-FAHM-P1/sw.js' : url = "/sw.js"
+    BASE_URL.startsWith('https:') ? url = '/20213-PWA-FAHM-P1/sw.js' : url = "/20213-PWA-FAHM-P1/sw.js"
     navigator.serviceWorker.register('/sw.js')
 }
